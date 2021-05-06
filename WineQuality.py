addb3acd402eab6b6510d5a50022227491a70496
Mehran <mehran.r88@gmail.com>
2021-05-06 12:54:34 -0600

    NN added

--- WineQuality.py
+++ WineQuality.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import sklearn.svm as svm
 from sklearn.linear_model import LogisticRegression
+from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.preprocessing import LabelEncoder
@@ -32,22 +33,27 @@ x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, train_size= 
 
 
 #Logistic Regression
-logReg_model = LogisticRegression()
+logReg_model = LogisticRegression(max_iter=20000)
 logReg_model.fit(x_train, y_train)
 logReg_predict = logReg_model.predict(x_test)
-
+print("\n\n\n im here")
 # SVM
-gamma = .005
+gamma = 1
 c = 1
-svm_model = svm.SVC(kernel='poly', C=c,gamma=gamma)
+svm_model = svm.SVC(kernel='linear', C=c,gamma=gamma)
 svm_model.fit(x_train, y_train)
 svm_predict = svm_model.predict(x_test)
+
+#Nueral Network
+NN = MLPClassifier(hidden_layer_sizes=50)
+NN.fit(x_train, y_train)
+NN_predict = NN.predict(x_test)
 
 results = 'Logistic Regression Results:\n\nConfusion Matrix:\n' + (metrics.confusion_matrix(y_test, logReg_predict)).__str__() + '\n'+ (metrics.classification_report(y_test, logReg_predict)).__str__() +'\n\n'
 
 results += 'SVM Results with gamma = ' + gamma.__str__()+ ' and C = '+ c.__str__() + '\n\nConfusion Matrix:\n' + (metrics.confusion_matrix(y_test, svm_predict)).__str__() + '\n' + (metrics.classification_report(y_test, svm_predict)).__str__() +'\n\n'
 
-print (results)
+print ((metrics.classification_report(y_test, NN_predict)))
 
 
 
