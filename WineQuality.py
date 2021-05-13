0ae6ab4f87d13cbcd0e8de46fec03ac861bb4fb2
Alanis Kitchen <alaniskitchen47@gmail.com>
2021-05-13 17:28:46 -0600

    Cleanup and labeling graph

--- WineQuality.py
+++ WineQuality.py
@@ -1,23 +1,24 @@
+#
+# Authors: Alanis Kitchen, Mehran Rahmanimehdiabadi
+# Date: 05/12/2021
+#
 import numpy as np
 import pandas as pd
-import sklearn.svm as svm
 from sklearn.linear_model import LogisticRegression
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn import metrics
-import sklearn.svm as svm 
+import sklearn.svm as svm
 import matplotlib.pyplot as plt
 
-
-    #reading dataset
+# reading dataset
 def get_dataset(all_data):
-    
-    # features = ["volatile acidity","residual sugar","chlorides","total sulfur dioxide","density","pH","sulphates","alcohol"]
-    # picking 5 random features to test different accuracies.
-    features = ["residual sugar","density","pH","alcohol"]
-    features2 = ["volatile acidity","chlorides","total sulfur dioxide","sulphates"]
-    features3 = ["volatile acidity","residual sugar","chlorides","total sulfur dioxide"]
+    # Allfeatures = ["volatile acidity","residual sugar","chlorides","total sulfur dioxide","density","pH","sulphates","alcohol"]
+    # picking random features to test different accuracies.
+    features = ["residual sugar", "density", "pH", "alcohol"]
+    features2 = ["volatile acidity", "chlorides", "total sulfur dioxide", "sulphates"]
+    features3 = ["volatile acidity", "residual sugar", "chlorides", "total sulfur dioxide"]
     features4 = ["residual sugar", "volatile acidity", "alcohol"]
     features5 = ["alcohol", "volatile acidity", "density", "chlorides"]
 
@@ -30,26 +31,16 @@ def get_dataset(all_data):
     for i in range(0, len(labels)):
         index = labels[i]
         count[index] += 1
-    
-
 
     return dataset, labels, count
 
 
-
-
 x_data, y_data, count = get_dataset("winequality-red.csv")
-#split data set
 
+# split data set
+x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, train_size=.75, shuffle=True, random_state=200)
 
-# plt.scatter(x_data, y_data, alpha=.5)
-# plt.show()
-
-x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, train_size= .75, shuffle=True, random_state=200)
- 
-
-
-#Logistic Regression
+# Logistic Regression
 logReg_model = LogisticRegression(max_iter=20000)
 logReg_model.fit(x_train, y_train)
 logReg_predict = logReg_model.predict(x_test)
@@ -57,25 +48,33 @@ logReg_predict = logReg_model.predict(x_test)
 # SVM
 gamma = 1
 c = 1
-svm_model = svm.SVC(kernel='linear', C=c,gamma=gamma, max_iter=20000)
+svm_model = svm.SVC(kernel='linear', C=c, gamma=gamma, max_iter=20000)
 svm_model.fit(x_train, y_train)
 svm_predict = svm_model.predict(x_test)
 
-#Nueral Network
+# Nueral Network
 layer_number = 75
 NN = MLPClassifier(hidden_layer_sizes=layer_number)
 NN.fit(x_train, y_train)
 NN_predict = NN.predict(x_test)
 
-results = 'Logistic Regression Results:\n\nConfusion Matrix:\n' + (metrics.confusion_matrix(y_test, logReg_predict)).__str__() + '\n'+ (metrics.classification_report(y_test, logReg_predict)).__str__() +'\n\n'
+results = 'Logistic Regression Results:\n\nConfusion Matrix:\n' + (
+    metrics.confusion_matrix(y_test, logReg_predict)).__str__() + '\n' + (
+              metrics.classification_report(y_test, logReg_predict)).__str__() + '\n\n'
 
-results += 'SVM Results with gamma = ' + gamma.__str__()+ ' and C = '+ c.__str__() + '\n\nConfusion Matrix:\n' + (metrics.confusion_matrix(y_test, svm_predict)).__str__() + '\n' + (metrics.classification_report(y_test, svm_predict)).__str__() +'\n\n'
+results += 'SVM Results with gamma = ' + gamma.__str__() + ' and C = ' + c.__str__() + '\n\nConfusion Matrix:\n' + (
+    metrics.confusion_matrix(y_test, svm_predict)).__str__() + '\n' + (
+               metrics.classification_report(y_test, svm_predict)).__str__() + '\n\n'
 
-results += 'NN Results with layer number of = ' + layer_number.__str__() + '\n\nConfusion Matrix:\n' + (metrics.confusion_matrix(y_test, NN_predict)).__str__() + '\n' + (metrics.classification_report(y_test, NN_predict)).__str__() +'\n\n'
+results += 'NN Results with layer number of = ' + layer_number.__str__() + '\n\nConfusion Matrix:\n' + (
+    metrics.confusion_matrix(y_test, NN_predict)).__str__() + '\n' + (
+               metrics.classification_report(y_test, NN_predict)).__str__() + '\n\n'
 
-print (results)
-plt.bar([i for i in range(0,9)], count, alpha=.5)
+print(results)
+plt.bar([i for i in range(0, 9)], count, alpha=.5, color="red")
+plt.xlabel("Wine Quality Rating")
+plt.ylabel("Number of Wines")
+plt.title("Wines per Rating in Dataset ")
 plt.show()
-
 
 
